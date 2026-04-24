7afba42df262e5d3348fd6032feb23109d2d7e32
MaryAlc2022 <mariana20septiembre@gmail.com>
2026-04-24 13:56:53 -0300

    bucke infinito

---
+++ EjercicioBucleInfinito/src/ejerciciobucleinfinito/EjercicioBucleInfinito.java
@@ -0,0 +1,21 @@
+
+package ejerciciobucleinfinito;
+
+
+public class EjercicioBucleInfinito {
+
+    
+    public static void main(String[] args) {
+        int contador=0;
+        
+        while (contador<10) {            
+            System.out.println("bucle infinito!!!!!!!!!");
+            System.out.println("bucle ");
+            contador++;
+            
+          
+        }
+        
+    }
+    
+}
